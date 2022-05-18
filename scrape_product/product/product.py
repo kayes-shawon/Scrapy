fe4af7b98b70001f14fa0d5a65ca233a6f624cfd
kayes <foridur.kayes@ucb.com.bd>
2022-05-18 17:20:01 +0600

    introducing beautiful soap

--- scrape_product/product/product.py
+++ scrape_product/product/product.py
@@ -1,5 +1,8 @@
+from bs4 import BeautifulSoup
+
 from scrape_base.base.api import BasePostAPI
 from scrape_product.code import product as product_code
+import requests
 
 
 class ScrapeProductAPI(BasePostAPI):
@@ -10,12 +13,20 @@ class ScrapeProductAPI(BasePostAPI):
         return 'ScrapeProductAPI'
 
     def process(self) -> tuple:
+        url = self.api_payload().get('scrape_url')
+        page = requests.get(url)
+
+        soup = BeautifulSoup(page.content, "html.parser")
+        images = [img for img in soup.findAll('img')]
+        print(str(len(images)) + " images found.")
+        print([i for i in images])
+        image_links = [each.get('src') for each in images]
         output = {
             'url': {
-                'link': "www.bata.com",
+                'link': url,
             },
             'products': {
                 'id': 1
-            }
+            },
         }
         return product_code.PRODUCT_SCRAPE_SUCCESS, output
